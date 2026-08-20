1de6fefc8463d392089543128df9fd805576c474
Sneha Barge <snehabarge90@gmail.com>
2026-08-21 02:19:50 +0530

    Connect frontend to deployed backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -32,7 +32,7 @@ function App() {
     setLoading(true);
 
     try {
-      const x = await fetch("http://127.0.0.1:8000/api/recommend", {
+      const x = await fetch("https://agentic-commerce-ai-assistant-1.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
